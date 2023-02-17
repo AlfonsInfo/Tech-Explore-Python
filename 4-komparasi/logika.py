877e6d17162e2823743ae19f845b6e988d0e38b1
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-02-17 11:04:48 +0700

    logika not, or, and ,xor

---
+++ 4-komparasi/logika.py
@@ -0,0 +1,17 @@
+# NOT
+print('=========NOT========')
+a = True
+c = not a
+print("data a = ", a)
+print("data c = ", c)
+# OR
+print('=========NOT========')
+a = True
+b = False
+c = a or b
+print("data a = ", a)
+print("data b = ", b)
+print("a OR b = ", c)
+
+c = a and b
+C = a ^ b  # untuk XOR
