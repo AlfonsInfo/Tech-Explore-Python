0c3dfab77c1c81d433878ce6dc31653093953304
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-02-17 13:58:46 +0700

    for dan while loop

---
+++ 8-controlling-flow/for-loop.py
@@ -0,0 +1,29 @@
+# Perulangan
+
+#* for kondisi :
+#*      aksi
+
+
+angka2= [0,2,4,6,8]
+
+for i in angka2:
+    print(f"i sekarang -> {i}")
+print("Akhir dari Program 1")
+
+
+angka2= range(5) #! 5 tidak termasuk!! dimulai dari 0-4
+
+for i in angka2:
+    print(f"i sekarang -> {i}")
+print("Akhir dari Program 2")
+
+# Menggunakan string
+
+data_string = " Saya Ganteng Abiesss"
+
+for huruf in data_string:
+    print(huruf)
+print("Akhir dari Program 2")
+
+#? Yang kayak bahasa pemrograman lain gimana ?
+#! for(int i = ;....) -> tidak bisa di for
---
+++ 8-controlling-flow/while-loop.py
@@ -0,0 +1,12 @@
+
+#! while kondisi 
+#       : aksi ini
+#       : aksi itu
+
+
+angka = 0 #! inisiasi angkanya disini!!
+while angka<5:
+    angka+=1 #! posisi ini tidak pengaurh terhadap loop kecuali ditaruh diluar loop
+    print(f"angka sekarang ->{angka}")
+    print("alfonss kece!")
+print("cukup!!!")
