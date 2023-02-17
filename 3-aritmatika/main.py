927d5d4b01465563da114927546af6fb42eb8f38
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-02-17 10:34:40 +0700

    Aritmatika & Operator Precedence

---
+++ 3-aritmatika/main.py
@@ -0,0 +1,42 @@
+#operasi aritmatika
+
+a = 10
+b = 3
+# tambah
+hasil = a+b
+print( a , '+', b , '=', hasil)
+# kurang
+hasil = a-b
+print( a , '-', b , '=', hasil)
+
+# Kali
+hasil = a*b
+print( a , '*', b , '=', hasil)
+
+#Bagi
+hasil = a/b
+print( a , '/', b , '=', hasil)
+
+#Eksponen
+hasil = a**b
+print( a , '**', b , '=', hasil)
+
+#Modulus
+hasil = a%b
+print( a , '%', b , '=', hasil)
+
+#Floor Division -> Pembulatan kebawah
+hasil = a//b
+print( a , '//', b , '=', hasil)
+
+# Prioritas Operasi / Operational Precedence
+x = 3
+y = 2
+z = 4
+hasil = x ** y * z + x / y - y % z // x
+print(x,'**',y,'*',z,'+',x,'/',y,'-',y,'%',z,'//',x,'=',hasil)
+
+# () -> OP
+# Eksponen
+# Perkalian / Pembagian
+# Pertambahan / Pengurangan
